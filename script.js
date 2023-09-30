196a0aa51c81522960d6a7fe60be6f4add3f2da6
leckliss <erickcaique150@hotmail.com>
2023-09-29 23:58:10 -0300

    finalizado pt1

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const elements = document.querySelectorAll('.hidden')
+
+
+
 const myObserver = new IntersectionObserver((entries) => {
     entries.forEach( (entry) => {
         if(entry.isIntersecting){
@@ -7,6 +11,22 @@ const myObserver = new IntersectionObserver((entries) => {
         }
     })
 })
-const elements = document.querySelectorAll('.hidden')
+
 
 elements.forEach((element) => myObserver.observe(element) )
+
+
+window.addEventListener("scroll", function() {
+    var menu = document.querySelector('header');
+    var logo = document.querySelector(".logo");
+
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 0) {
+        menu.classList.add("scrolled");
+        logo.classList.add("scrolled");
+    } else {
+        menu.classList.remove("scrolled");
+        logo.classList.remove("scrolled");
+    }
+});
