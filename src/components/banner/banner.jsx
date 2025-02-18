977917e3bf2f91229a6fdf8c977685f03465ac03
leckliss <erickcaique150@hotmail.com>
2025-02-18 00:39:21 -0300

    incluindo texto animacao banner

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,30 +1,45 @@
 import ImgPrincipal from '../../assets/imgs/home/imgPrincipal.svg';
 
-import { FaWhatsapp} from 'react-icons/fa';
+import { FaWhatsapp } from 'react-icons/fa';
+import SplitText from "./SplitText";
 
 
 import './banner.css';
-
+const handleAnimationComplete = () => {
+    console.log('All letters have animated!');
+  };
+  
 export default function Banner() {
     return (
-       <>
-        
-        <main>
-            <section className='introduction-container'>
-                <div className='introduction-container__actions-container'>
-                    <h1>MARQUE SUA SESSÃO COMIGO HOJE MESMO</h1>
-                    <button>
-                        AGENDAR SERVIÇO 
-                        <FaWhatsapp size={36} color='#000' />
-                    </button>
-                </div>
-                <div className='introduction-container__img-container'> 
-                    <img src={ImgPrincipal} />
-                </div>
-            </section>
-        </main>
-        
-       </>
-       
+        <>
+
+            <main>
+                <section className='introduction-container'>
+                    <div className='introduction-container__actions-container'>
+                        <SplitText
+                            text="Oi! Amiga!"
+                            className="text-2xl font-semibold text-center"
+                            delay={130}
+                            animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+                            animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+                            easing="easeInOutCubic"
+                            threshold={0.2}
+                            rootMargin="-50px"
+                            onLetterAnimationComplete={handleAnimationComplete}
+                        />
+                        <h1>Marque sua sessão comigo hoje mesmo!</h1>
+                        <button>
+                            AGENDAR SERVIÇO
+                            <FaWhatsapp size={36} color='#000' />
+                        </button>
+                    </div>
+                    <div className='introduction-container__img-container'>
+                        <img src={ImgPrincipal} />
+                    </div>
+                </section>
+            </main>
+
+        </>
+
     );
 }
